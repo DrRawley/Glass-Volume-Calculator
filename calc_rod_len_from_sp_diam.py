f155b0027ccaf435b8ee1b45a6f460d3a574b3ab
Rawley Greene <rawley.greene@gmail.com>
2025-07-10 01:31:26 +0000

    minor formatting update

--- calc_rod_len_from_sp_diam.py
+++ calc_rod_len_from_sp_diam.py
@@ -6,8 +6,12 @@ st.title("Calculate Rod Length from Sphere Diameter")
 sphere_diam = st.number_input('Sphere Diameter (mm): ', min_value=0.0, max_value=1000.0, step=0.1, value=10.0)
 sphere_vol = math.pi / 6 * sphere_diam ** 3
 st.write(f"Sphere volume = {sphere_vol:.1f} mm^3")
+st.divider()
 
-rod_diameter = st.number_input('Rod Diamter (mm): ', min_value=0.01, max_value=1000.0, value=7.0) #, key=f"num_{i}")
+#Rod row
+col_diam, col_len = st.columns([0.3,0.7], vertical_alignment="bottom")
+rod_diameter = col_diam.number_input('Rod Diamter (mm): ', min_value=0.01, max_value=1000.0, value=7.0) #, key=f"num_{i}")
 
 rod_length = sphere_vol / ( math.pi / 4 * rod_diameter ** 2 )
-st.write(f"Rod length = {rod_length:.1f} mm")
+col_len.subheader(f"Rod length = {rod_length:.1f} mm")
+#End Rod row
