6320ea89c01dee68ec19077f80895d4ce08305eb
Rawley Greene <rawley.greene@gmail.com>
2025-07-10 00:18:42 +0000

    add navigation to multiple calculators

---
+++ calc_sphere_diam.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import math
+
+st.title("Calculate Glass Sphere Diameter")
+st.divider()
+
+num_rods = st.number_input('Number of rods: ', min_value=1, max_value=8, step=1, value=1)
+st.divider()
+
+rod_diam = []
+rod_length = []
+rod_volume = []
+
+
+for i in range(num_rods):
+    col_diam, col_len = st.columns([0.3,0.7])
+    rod_diam.append(col_diam.number_input('Rod Diamter (mm): ', min_value=0.01, value=7.0, key=f"num_{i}"))
+    rod_length.append(col_len.slider('Rod Length (mm): ', min_value=0.0, max_value=100.0, step=0.1, key=f"slider_{i}"))
+
+    #rod_diam.append(st.number_input('Rod Diamter (mm): ', min_value=0.01, value=7.0, key=f"num_{i}"))
+    #rod_length.append(st.slider('Rod Length (mm): ', min_value=0.0, max_value=100.0, step=0.1, key=f"slider_{i}"))
+    
+    rod_volume.append(math.pi / 4 * rod_diam[i] ** 2 * rod_length[i])
+    st.write(f"Rod {i + 1} Volume is {rod_volume[i]:.2f} mm^3.")
+
+
+total_volume = sum(rod_volume)
+
+st.divider()
+st.write(f"Total Volume is {total_volume:.2f} mm^3.")
+sphere_diameter = 2 * ( 3 * total_volume / ( 4 * math.pi ) ) ** (1/3)
+st.header(f"Sphere diameter is {sphere_diameter:.2f} mm.")
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,17 @@
 import streamlit as st
 import math
 
-st.title("🎈 Glass Volume Calculator")
-#st.write("Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+def page_2():
+    st.title("Page 2")
 
-num_rods = st.number_input('Number of rods: ', min_value=1, max_value=8, step=1, value=1)
+pages = {
+    "Calculators": [
+    st.Page("calc_sphere_diam.py", title="Calculate Sphere Diameter"),
+    st.Page(page_2, title="Page 2")
+    ]
+}
 
-rod_diam = []
-rod_length = []
-rod_volume = []
-
-
-for i in range(num_rods):
-    rod_diam.append(st.number_input('Rod Diamter (mm): ', min_value=0.01, value=7.0, key=f"num_{i}"))
-    rod_length.append(st.slider('Rod Length (mm): ', min_value=0.0, max_value=100.0, step=0.1, key=f"slider_{i}"))
-    rod_volume.append(math.pi / 4 * rod_diam[i] ** 2 * rod_length[i])
-    st.write(f"Rod {i + 1} Volume is {rod_volume[i]:.2f} mm^3.")
+pg = st.navigation(pages)
+pg.run()
 
 
-total_volume = sum(rod_volume)
-
-st.write(f"Total Volume is {total_volume:.2f} mm^3.")
-sphere_diameter = 2 * ( 3 * total_volume / ( 4 * math.pi ) ) ** (1/3)
-st.write(f"Sphere diameter is {sphere_diameter:.2f} mm.")
